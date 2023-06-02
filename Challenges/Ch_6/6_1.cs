7c60a2d49e06b4d5e3583110a199befbe3a3d30f
matthew <matthewtbyron@protonmail.com>
2023-06-02 09:18:28 -0400

    completed chapter 6

---
+++ Challenges/Ch_6/6_1.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+int a = 10000;
+short b = 500;
+long c = 500000;
+byte d = 30;
+sbyte e = -80;
+uint f = 9000;
+ushort g = 1500;
+ulong h = 100000000;
+char i = 'G';
+string j = "FREE";
+float k = 2.0f;
+double l = 3.0f;
+decimal m = 5.0m;
+bool isValid = true;
+
+//updates to values
+a = 7;
+b = 7;
+c = 7;
+d = 7;
+e = 7;
+f = 7;
+g = 7;
+h = 7;
+i = '7';
+j = "7";
+k = 7.0f;
+l = 4.0;
+m = 7.0m;
+isValid = false;
+    
+
+
+Console.WriteLine("Varible Shop Prices:");
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
+Console.WriteLine(m);
+Console.WriteLine(isValid);
+Console.ReadLine();
